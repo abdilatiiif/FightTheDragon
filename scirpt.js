2e18b00d9c9f9ac02c63709595284974542b72ed
Latif Hassan <73110623+abdilatiiif@users.noreply.github.com>
2024-01-30 20:05:33 +0100

    Add files via upload

--- scirpt.js
+++ scirpt.js
@@ -10,11 +10,10 @@ const dragonName = document.getElementById("dragon-name-txt");
 const healerHealth = document.getElementById("healer-health-txt");
 const archerHealth = document.getElementById("archer-health-txt");
 const warriorHealth = document.getElementById("warrior-health-txt");
-const dragonHealth = document.getElementById("dragon-health-txt");
+const dragonHealth = document.querySelector(".dragon-health-txt");
 
 //heros DOM
 const playersDOM = document.querySelectorAll(".hero");
-console.log(playersDOM);
 
 //Stats for heroes
 const heroesArray = [
@@ -31,7 +30,7 @@ const heroesArray = [
     id: 1,
     name: "Ariana archer",
     maxHP: 500,
-    currentHP: 500,
+    currentHP: 900,
     damage: 400,
     alive: true,
     playerDOM: '<div class="img-container archer">',
@@ -40,7 +39,7 @@ const heroesArray = [
     id: 2,
     name: "Wyona Warrior",
     maxHP: 600,
-    currentHP: 600,
+    currentHP: 700,
     damage: 400,
     alive: true,
     playerDOM: '<div class="img-container warrior">',
@@ -64,7 +63,6 @@ dragonName.textContent = dragonObject.name;
 
 //gi heroImg Id
 const heroID = [heroesArray[0].id, heroesArray[1].id, heroesArray[2].id];
-console.log(heroID);
 
 playersDOM.forEach((player, i) => {
   player.id = i;
@@ -95,13 +93,29 @@ function attack (e) {
 */
 
 function attack(e) {
-  console.log(
-    ` ${heroesArray[e.target.id].name} har gjort ${
-      heroesArray[e.target.id].damage
-    } skade på ${dragonObject.name}!`
-  );
-  dragonObject.currentHP -= heroesArray[e.target.id].damage;
-  console.log(dragonObject.currentHP);
+  // hvis helten lever, kan angripe dragen
+  if (heroesArray[e.target.id].alive) {
+    alert(
+      ` ${heroesArray[e.target.id].name} har gjort ${
+        heroesArray[e.target.id].damage
+      } skade på ${dragonObject.name}!`
+    );
+    dragonObject.currentHP -= heroesArray[e.target.id].damage;
+    console.log(dragonObject.currentHP);
+    dragonHealth.textContent = dragonObject.currentHP;
+  }
+
+  // dragen angriper
+  if (dragonObject.currentHP >= 0) {
+    dragonAttack();
+    healerHealth.textContent = `${heroesArray[0].currentHP} HP`;
+    archerHealth.textContent = `${heroesArray[1].currentHP} HP`;
+    warriorHealth.textContent = `${heroesArray[2].currentHP} HP`;
+  } else {
+    alert("Dragen er død 🎉");
+    alert("Gratulerer, du har vunnet spillet!🎉");
+    document.querySelector(".dragon").remove();
+  }
 }
 
 /*
@@ -114,4 +128,23 @@ playersDOM.forEach((player) => {
   player.addEventListener("click", attack);
 });
 
-function dragonAttack() {}
+function dragonAttack() {
+  let randomHero = Math.floor(Math.random() * 3);
+
+  if (heroesArray[randomHero].alive) {
+    heroesArray[randomHero].currentHP -= dragonObject.damage;
+    alert(
+      `${dragonObject.name} har gjort ${dragonObject.damage} damage på ${heroesArray[randomHero].name}`
+    );
+    console.log(
+      heroesArray[randomHero].name,
+      heroesArray[randomHero].currentHP
+    );
+
+    if (heroesArray[randomHero].currentHP <= 0) {
+      heroesArray[randomHero].alive = false;
+      document.getElementById(randomHero).parentElement.remove();
+      alert(`${heroesArray[randomHero].name} is dead`);
+    }
+  }
+}
